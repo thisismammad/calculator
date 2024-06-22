6f6ef9f350182c5ad8eb70d6f2fc158f2ec1c6e8
Mammad <thisismammad7@gmail.com>
2024-06-22 21:35:32 +0330

    fixed zero before number

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const numbers = document.querySelectorAll('.number');
-const phrase = document.querySelector('.phrase')
-const result = document.querySelector('.result')
-const ac = document.querySelector('.ac');
-const bs = document.querySelector('.bs');
-const operators = document.querySelectorAll('.operator');
-const equal = document.querySelector('.equal');
-
+const numbers = document.querySelectorAll(".number");
+const phrase = document.querySelector(".phrase");
+const result = document.querySelector(".result");
+const ac = document.querySelector(".ac");
+const bs = document.querySelector(".bs");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
 
 clickedEqual = false;
 let num1 = 0;
@@ -16,144 +15,156 @@ let secondNumClick = false;
 
 clickedOprator = false;
 for (const number of numbers) {
-    number.addEventListener('click', () => {
-        if (clickedEqual) {
-            reset();
-        }
-        clickedEqual = false;
-        if (phrase.textContent === "0") {
-            phrase.textContent = "";
-        }
+  number.addEventListener("click", () => {
+    if (clickedEqual) {
+      reset();
+    }
+    clickedEqual = false;
+    if (phrase.textContent === "0") {
+      phrase.textContent = "";
+    }
 
-        if (phrase.textContent === "" && number.textContent === ".") {
-            phrase.textContent = "0.";
-        }
-        if (number.textContent === "." && (!phrase.textContent.includes(".") || (phrase.textContent.lastIndexOf(".") < phrase.textContent.lastIndexOf(op)))) {
+    if (phrase.textContent === "" && number.textContent === ".") {
+      phrase.textContent = "0.";
+    }
+    if (
+      number.textContent === "." &&
+      (!phrase.textContent.includes(".") ||
+        phrase.textContent.lastIndexOf(".") <
+          phrase.textContent.lastIndexOf(op))
+    ) {
+      phrase.textContent += number.textContent;
+    } else if (number.textContent !== ".") {
+      if (number.textContent === "0") {
+        if (phrase.textContent[phrase.textContent.length - 1] === "0") {
+          if (
+            isNaN(phrase.textContent[phrase.textContent.length - 2]) ===
+              false ||
+            phrase.textContent[phrase.textContent.length - 2] === "."
+          ) {
             phrase.textContent += number.textContent;
-        } else if (number.textContent !== ".") {
-            phrase.textContent += number.textContent;
+          }
+        } else {
+          phrase.textContent += number.textContent;
         }
-        if (clickedOprator) {
-            calculator();
-            clickedOprator = false;
-            secondNumClick = true;
-        }
-    });
-
+      } else {
+        phrase.textContent += number.textContent;
+      }
+    }
+    if (clickedOprator) {
+      calculator();
+      clickedOprator = false;
+      secondNumClick = true;
+    }
+  });
 }
 
-ac.addEventListener('click', () => {
-    reset();
+ac.addEventListener("click", () => {
+  reset();
 });
 
-bs.addEventListener('click', () => {
-    if (isNaN(phrase.textContent.charAt(phrase.textContent.length - 1))) {
-        clickedOprator = false;
-    }
-    phrase.textContent = phrase.textContent.slice(0, -1);
-    result.textContent = "";
-    clickedEqual = false;
-    if (phrase.textContent.split(op).length === 1) {
-        ans = Number(phrase.textContent);
-    }
-    if (phrase.textContent === "") {
-        phrase.textContent = "0";
-        reset();
-    }
+bs.addEventListener("click", () => {
+  if (isNaN(phrase.textContent.charAt(phrase.textContent.length - 1))) {
+    clickedOprator = false;
+  }
+  phrase.textContent = phrase.textContent.slice(0, -1);
+  result.textContent = "";
+  clickedEqual = false;
+  if (phrase.textContent.split(op).length === 1) {
+    ans = Number(phrase.textContent);
+  }
+  if (phrase.textContent === "") {
+    phrase.textContent = "0";
+    reset();
+  }
 
-    if (ans === "Can't divide by zero") {
-        reset();
-    }
+  if (ans === "Can't divide by zero") {
+    reset();
+  }
 });
 
 for (const operator of operators) {
-    operator.addEventListener('click', () => {
-        if (clickedEqual) {
-            phrase.textContent = ans;
-            result.textContent = "";
-            clickedEqual = false;
-        }
-        if (secondNumClick) {
-            phrase.textContent = ans;
-            secondNumClick = false;
-        }
-        if (!clickedOprator) {
-            phrase.textContent += operator.textContent;
-            clickedOprator = true;
-            op = operator.textContent;
-
-        } else if (op !== operator.textContent) {
-            phrase.textContent = phrase.textContent.slice(0, -1);
-            phrase.textContent += operator.textContent;
-            op = operator.textContent;
-        }
-
-    });
+  operator.addEventListener("click", () => {
+    if (clickedEqual) {
+      phrase.textContent = ans;
+      result.textContent = "";
+      clickedEqual = false;
+    }
+    if (secondNumClick) {
+      phrase.textContent = ans;
+      secondNumClick = false;
+    }
+    if (!clickedOprator) {
+      phrase.textContent += operator.textContent;
+      clickedOprator = true;
+      op = operator.textContent;
+    } else if (op !== operator.textContent) {
+      phrase.textContent = phrase.textContent.slice(0, -1);
+      phrase.textContent += operator.textContent;
+      op = operator.textContent;
+    }
+  });
 }
 
-
-equal.addEventListener('click', () => {
-    if (!clickedEqual) {
-        let sp = phrase.textContent.split(op);
-        if (sp[1] === undefined) {
-            result.textContent = phrase.textContent;
-            phrase.textContent += equal.textContent;
-            ans = Number(result.textContent);
-        } else if (sp[1] === '') {
-            phrase.textContent = phrase.textContent.slice(0, -1);
-            result.textContent = phrase.textContent;
-            phrase.textContent += equal.textContent;
-            ans = Number(result.textContent);
-        } else {
-            calculator();
-            phrase.textContent += equal.textContent;
-            result.textContent = ans;
-        }
-
+equal.addEventListener("click", () => {
+  if (!clickedEqual) {
+    let sp = phrase.textContent.split(op);
+    if (sp[1] === undefined) {
+      result.textContent = phrase.textContent;
+      phrase.textContent += equal.textContent;
+      ans = Number(result.textContent);
+    } else if (sp[1] === "") {
+      phrase.textContent = phrase.textContent.slice(0, -1);
+      result.textContent = phrase.textContent;
+      phrase.textContent += equal.textContent;
+      ans = Number(result.textContent);
+    } else {
+      calculator();
+      phrase.textContent += equal.textContent;
+      result.textContent = ans;
     }
-    clickedEqual = true;
-    clickedOprator = false;
-
+  }
+  clickedEqual = true;
+  clickedOprator = false;
 });
 
-
 function reset() {
-    phrase.textContent = "0";
-    result.textContent = "";
-    clickedOprator = false;
-    clickedEqual = false;
-    secondNumClick = false;
-    num1 = 0;
-    num2 = 0;
-    ans = 0;
-    op = "+";
+  phrase.textContent = "0";
+  result.textContent = "";
+  clickedOprator = false;
+  clickedEqual = false;
+  secondNumClick = false;
+  num1 = 0;
+  num2 = 0;
+  ans = 0;
+  op = "+";
 }
 
 function calculator() {
-    let index = phrase.textContent.lastIndexOf(op);
-    num1 = Number.parseFloat(phrase.textContent.substring(0, index));
-    num2 = Number.parseFloat(phrase.textContent.substring(index + 1));
-    switch (op) {
-        case "/":
-            ans = num1 / num2;
-            break;
-        case "*":
-            ans = num1 * num2;
-            break;
-        case "-":
-            ans = num1 - num2;
-            break;
-        case "+":
-            ans = num1 + num2;
-            break;
-        default:
-            ans = "Invalid";
-    }
-    clickedOprator = false;
-    if (ans === Infinity || isNaN(ans)) {
-        ans = "Can't divide by zero";
-    } else if (!Number.isInteger(ans) && ans.toString().length > 10) {
-        ans = ans.toFixed(10);
-    }
-    return ans
+  let index = phrase.textContent.lastIndexOf(op);
+  num1 = Number.parseFloat(phrase.textContent.substring(0, index));
+  num2 = Number.parseFloat(phrase.textContent.substring(index + 1));
+  switch (op) {
+    case "/":
+      ans = num1 / num2;
+      break;
+    case "*":
+      ans = num1 * num2;
+      break;
+    case "-":
+      ans = num1 - num2;
+      break;
+    case "+":
+      ans = num1 + num2;
+      break;
+    default:
+      ans = "Invalid";
+  }
+  clickedOprator = false;
+  if (ans === Infinity || isNaN(ans)) {
+    ans = "Can't divide by zero";
+  } else if (!Number.isInteger(ans) && ans.toString().length > 10) {
+    ans = ans.toFixed(10);
+  }
+  return ans;
 }
